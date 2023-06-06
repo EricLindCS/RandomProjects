d1e4fdf304c43d7f384f3fbb8b5bc67f759df581
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-06-06 08:01:06 -0700

    Update sketch_jun1a_copy_20230605122009_copy_20230606074550.ino
    Added comments and cleaned up the code

--- RupeGoldpergSTM32/sketch_jun1a_copy_20230605122009_copy_20230606074550/sketch_jun1a_copy_20230605122009_copy_20230606074550.ino
+++ RupeGoldpergSTM32/sketch_jun1a_copy_20230605122009_copy_20230606074550/sketch_jun1a_copy_20230605122009_copy_20230606074550.ino
@@ -1,18 +1,22 @@
-#define BUTTON_PIN PB5
-#define ENDPNEU PB8
-#define RAMPPNEU PB7
-#define LED_BUILTIN PC13
-#define BUTTON_PIN_2 PB9
+//pin defintions (STM32)
+//Not required, but makes it easier to keep track of what does what
+
+#define BUTTON_PIN PB5 //Contact pin 1
+#define ENDPNEU PB8 //Relay out (single action solonoid)
+#define RAMPPNEU PB7 //Relay out (double-action solonoid)
+#define LED_BUILTIN PC13 //Board's built in LED
+#define BUTTON_PIN_2 PB9 //Contact pin 2
 
 void setup() {
   
+  //Pin setups
   pinMode(ENDPNEU, OUTPUT);
-  //pinMode(PB5, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(RAMPPNEU, OUTPUT);
   pinMode(BUTTON_PIN_2, INPUT_PULLUP);
 
+  //Reset double action pneumatics cycle (with visual indicator)
   digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(RAMPPNEU, HIGH);
   delay(1000);
@@ -25,21 +29,22 @@ void setup() {
 
 void loop() {
 
-  // put your main code here, to run repeatedly:
-  if(!digitalRead(BUTTON_PIN)){
-    delay(1000);
-    digitalWrite(RAMPPNEU, HIGH);
-    delay(3000);
-    digitalWrite(RAMPPNEU, LOW);
+  // Ramp pneumatics relay handeler
+  if(!digitalRead(BUTTON_PIN)){ //See if ball is completing the circut
+    delay(1000); //Give the ball time to stop
+    digitalWrite(RAMPPNEU, HIGH); //Actuate piston
+    delay(3000); //Give the ball time to roll down ramp
+
+    digitalWrite(RAMPPNEU, LOW); //Re-lower ramp for testing purposes, !!!remove later!!!
   }
 
-  if(digitalRead(BUTTON_PIN_2)){
-    digitalWrite(ENDPNEU, HIGH);
+  if(digitalRead(BUTTON_PIN_2)){ //See if ball 2 is completing the other circut
+    digitalWrite(ENDPNEU, HIGH); //Actuate piston
   }
   else {
-    digitalWrite(ENDPNEU, LOW);
+    digitalWrite(ENDPNEU, LOW); //Release Piston
   }
 
-  delay(200);
+  delay(200); //delay between each cycle of the loop
   
 }
