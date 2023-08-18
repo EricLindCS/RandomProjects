98a8d799b666cc0ae3d1c277f71699a122c6b34f
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2023-08-18 12:30:19 -0700

    Added readme/more comments

--- Potentiometer/Potentiometer.ino
+++ Potentiometer/Potentiometer.ino
@@ -1,8 +1,7 @@
-int potetn = 0; //global variable, not nessesary here but why not
-
+int _potetn = 0; //global variable, not nessesary here but why not
 
 //define pins using phrases, not nessesary, but good practice
-#define POTPIN 0
+#define POTENTIOMETER 0
 #define LED1 9
 #define LED2 10
 #define LED3 11
@@ -26,10 +25,10 @@ void lightup(int num){
 //runs at start of program, defines output pins (do not need to define analog input pins)
 void setup()
 {
-    pinMode(10, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(12, OUTPUT);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
 
     //begin serial connection to computer for debugging
     Serial.begin(9600); 
@@ -38,18 +37,18 @@ void setup()
 //Constantly iterates
 void loop()
 {
-    potetn = analogRead(0); //get potentiometer value, 0 to 1023
+    _potetn = analogRead(POTENTIOMETER); //get potentiometer value, 0 to 1023
 
-//ignore this
-    //Serial.write(potetn.ToString());
+    //ignore this//
+    //Serial.write(_potetn.ToString());
     //Serial.write(" & ");
 
-    //do math to change potentiometer value to either 0,1,2,3,4 and print that value to console
-    potetn = map(potetn, 0, 1023, 0, 4);
-    Serial.println(potetn);
+    //do math to change potentiometer value from 0-1023 to either 0,1,2,3,4 and print that value to console
+    _potetn = map(_potetn, 0, 1023, 0, 4);
+    Serial.println(_potetn);
    
    //switch statement based on 0-4 value
-    switch (potetn)
+    switch (_potetn)
     {
     case 0:
         lightup(0);
@@ -69,6 +68,9 @@ void loop()
     default:
         break;
     }
+
+    /*instead of the code block above, you could just write this*/
+    //lightup(_potetn);
 
     delay(100); //delay between iterations of the program
 }
---
+++ Potentiometer/readme.md
@@ -0,0 +1,11 @@
+Arduino | Device
+--- | --- 
+A0 | Potentiometer White
+5V | Potentiometer Red
+GND | Potentiometer Black
+5V | Circus 8
+GND | Circus 7
+9 | Cricus 6
+10 | Cricus 5
+11 | Cricus 4
+12 | Cricus 3
